d238870820841bd6456817b405fc805e4908b1a8
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2019-02-17 15:20:45 -0600

    Code Clean up

--- src/main/java/com/unrealdinnerbone/leathercows/item/DropCalculatorItem.java
+++ src/main/java/com/unrealdinnerbone/leathercows/item/DropCalculatorItem.java
@@ -15,14 +15,15 @@ public class DropCalculatorItem extends Item {
     public DropCalculatorItem() {
         super(new Settings().itemGroup(LeatherCowItems.itemGroup));
     }
-    public static void onEntityClicked(PlayerEntity playerEntity, Hand hand, MobEntity aThis) {
-        if(!playerEntity.world.isClient()) {
+
+    public static void onEntityClicked(PlayerEntity playerEntity, Hand hand, MobEntity mobEntity) {
+        if (!playerEntity.world.isClient()) {
             System.out.println(playerEntity.getStackInHand(hand).getItem());
             if (playerEntity.getStackInHand(hand).getItem() instanceof DropCalculatorItem)
-                if (aThis instanceof IDropperItemEntity) {
-                    IDropperItemEntity iDropperItemEntity = (IDropperItemEntity) aThis;
-                    TextComponent textComponent = new StringTextComponent("Next ").append(new TranslatableTextComponent(iDropperItemEntity.getLCItemDrooped().getTranslationKey()).append(new StringTextComponent( " Drop in " + iDropperItemEntity.getLCTimeTillNextDrop() / 20  + "s")));
-                    playerEntity.addChatMessage(textComponent, true );
+                if (mobEntity instanceof IDropperItemEntity) {
+                    IDropperItemEntity iDropperItemEntity = (IDropperItemEntity) mobEntity;
+                    TextComponent textComponent = new StringTextComponent("Next ").append(new TranslatableTextComponent(iDropperItemEntity.getLCItemDrooped().getTranslationKey()).append(new StringTextComponent(" Drop in " + iDropperItemEntity.getLCTimeTillNextDrop() / 20 + "s")));
+                    playerEntity.addChatMessage(textComponent, true);
                 }
         }
     }
